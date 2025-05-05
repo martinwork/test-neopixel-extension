0168d56c31a4da2f7e49d554ae34edf9b50208a4
Martin Williams <martinwork@users.noreply.github.com>
2025-05-05 16:18:35 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+let count = 0
 basic.forever(function () {
-    strip.rotate(1)
-    strip.show()
-    basic.pause(100)
+    led.toggle(0, 0)
+    basic.pause(500)
+})
+control.inBackground(function () {
+    while (true) {
+        count += 1
+        strip.setPixelColor(0, neopixel.rgb(0, count, 0))
+        strip.show()
+        control.waitMicros(200)
+    }
 })
